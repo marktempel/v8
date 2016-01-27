a8ddfb1e6d74ef8a96cd01e45455e0343fd93994
Mark Howarth <mark.howarth@landesk.com>
2016-01-27 14:58:02 -0700

    Added copyright, removed some commented lines and added a couple of comments

--- ld-v8setup.bat
+++ ld-v8setup.bat
@@ -1,3 +1,5 @@
+echo Copyright (c) 2016 LANDESK Software Inc. All rights reserved.
+
 echo off
 setlocal
 for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
@@ -22,13 +24,16 @@ call fetch v8
 SET DEPOT_TOOLS_WIN_TOOLCHAIN=0
 SET GYP_MSVS_VERSION=2015
 
+REM favor size instead of speed
 powershell -Command "(Get-Content '%cd%\v8\build\toolchain.gypi') | Foreach-Object {$_ -ireplace '''Optimization''\s*:\s*''\d''', '''Optimization'': ''1'''} | Set-Content  '%cd%\v8\build\toolchain.gypi'"
 powershell -Command "(Get-Content '%cd%\v8\build\toolchain.gypi') | Foreach-Object {$_ -ireplace '''FavorSizeOrSpeed''\s*:\s*''\d''', '''FavorSizeOrSpeed'': ''2'''} | Set-Content  '%cd%\v8\build\toolchain.gypi'"
-REM call %cd%\depot_tools\python276_bin\python .\ld-favorsize.py .\v8\build\toolchain.gypi
 
-REM call "%cd%\depot_tools\python276_bin\python" %cd%\v8\build\gyp_v8 -Dv8_enable_i18n_support=0 -Dv8_use_snapshot=0 -Dcomponent=shared_library
+REM v8_enable_i18n_support=0, remove internationalization support
+REM v8_use_snapshot=0, saves a little space but is slower to load
+REM component=shared_library, build as a DLL, remove this to build as a static library
 "%cd%\depot_tools\python276_bin\python" %cd%\v8\build\gyp_v8 -Dv8_enable_i18n_support=0 -Dcomponent=shared_library
 
+REM Do not treat linker warnings as errors
 %cd%\depot_tools\python276_bin\python .\ld-turnoffwarnings.py
 
 msbuild.exe "%cd%\v8\tools\gyp\v8.sln" /p:configuration=release
